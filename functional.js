2b32f8bb947eac4e8cd0396f3a6fc3071e07f286
Lee Crossley <leee@hotmail.co.uk>
2013-08-09 14:24:51 +0100

    Functional each

--- functional.js
+++ functional.js
@@ -1,5 +1,14 @@
 var λ = (function () {
     var λ = {};
-
+    
+    λ.each = function (iterator, items) {
+        if (!items || typeof (iterator) !== "function") {
+            return;
+        }
+        for (var i = 0; i < items.length; i++) {
+            iterator(items[i]);
+        }
+    };
+    
     return λ;
 })();
--- spec.js
+++ spec.js
@@ -2,4 +2,17 @@ describe("functional", function() {
     it("should have a global λ object", function() {
         expect(λ).toBeDefined();
     });
+    
+    it("should be able to add items to an array using λ.each", function() {
+        var result = [],
+            items = ["f", "u", "n", "c"];
+
+        var addTo = function (item) {
+            return result.push(item);
+        };
+
+        λ.each(addTo, items);
+        
+        expect(result).toEqual(items);
+    });
 });
