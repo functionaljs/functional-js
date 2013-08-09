bedaab6b8672a8dbec7e6580be30e144ec8202a1
Lee Crossley <leee@hotmail.co.uk>
2013-08-09 15:02:32 +0100

    Red, proper TDD man

--- functional.js
+++ functional.js
@@ -1,6 +1,14 @@
 var λ = (function () {
     var λ = {};
     
+    λ.curry = function (func) {
+        if (!func || typeof (func) !== "function") {
+            throw "λ Error: No function to curry";
+        }
+        // TODO actually analyse args and do the curry
+        return func;
+    };
+    
     λ.each = function (iterator, items) {
         if (!items || typeof (iterator) !== "function") {
             return;
--- spec.js
+++ spec.js
@@ -3,6 +3,25 @@ describe("functional", function() {
         expect(λ).toBeDefined();
     });
     
+    it("should throw an error attempting to λ.curry a string", function() {
+        var result = function () {
+            λ.curry("I am a string");
+        };
+        
+        expect(result).toThrow("λ Error: No function to curry");
+    });
+    
+    it("should λ.curry a string concatenation function", function() {
+        var concatenate = λ.curry(function(word1, word2) {
+            return word1 + " " + word2;
+        }); 
+
+        var concatenateHello = concatenate("Hello");
+        var result = concatenateHello("World");
+        
+        expect(result).toEqual("Hello World");
+    });
+    
     it("should be able to add items to an array using λ.each", function() {
         var result = [],
             items = ["f", "u", "n", "c"];
